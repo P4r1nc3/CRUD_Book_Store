7440f0dd6773ce7d962cd3b1190a78a840b80392
parince <konrad.tupta@gmail.com>
2023-03-04 17:55:17 +0100

    Added book entity

---
+++ src/main/java/com/bookStore/entity/Book.java
@@ -0,0 +1,27 @@
+package com.bookStore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String author;
+    private String price;
+
+    public Book(int id, String name, String author, String price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book() {
+    }
+}
