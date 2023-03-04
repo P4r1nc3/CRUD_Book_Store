2a519d76fb58be0d7f8c94576f9d07ca3bfdbe8c
parince <konrad.tupta@gmail.com>
2023-03-04 18:02:55 +0100

    Added book register

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
-    @GetMapping
+    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
     }
 }
